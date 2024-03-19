ab4788d2aed7fb1e94543b59b71398b711a4fa22
Juhyeon Jeong <wwg6710@naver.com>
2024-03-19 10:00:52 +0900

    접미사인지 확인하기

---
+++ 접미사인지확인하기.py
@@ -0,0 +1,7 @@
+def solution(my_string, is_suffix):
+    for i in range(len(is_suffix)):
+        if my_string[-len(is_suffix):] == is_suffix:
+            answer = 1
+        else:
+            answer = 0
+    return answer
